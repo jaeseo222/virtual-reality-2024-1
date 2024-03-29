fad47d88f28f030e1c7535f3f046e97d5068b2aa
jaeseo <jaeserrr@ewhain.net>
2024-03-29 12:31:11 +0900

    [FEAT] W04-1| loadScene, Door Animaion

---
+++ Assets/W04_LoadScene_Lecture/W04_Load_Scene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class W04_Load_Scene : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Robot_Transform") {
+            SceneManager.LoadScene("W04_Scene2");
+        }
+    }
+}
